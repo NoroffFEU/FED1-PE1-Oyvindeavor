58241693f12a8e6d1bb05c8ed207dc1cba916cc8
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-03 02:28:50 +0200

    Update constants.mjs

---
+++ js/constants.mjs
@@ -0,0 +1,6 @@
+// Added a constant here for the api so that its easily changeable if needed
+const noroffAPIURL = 'https://v2.api.noroff.dev';
+
+// use a different constant for usage so that the main constant is not changed by mistake
+const apiUrl =  noroffAPIURL;
+export { apiUrl };
