aae8a1d8a0fce4094a8f91c5787e5b9cee762776
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-03 10:47:28 +0200

    Update carousel.mjs

--- js/components/carousel.mjs
+++ js/components/carousel.mjs
@@ -1,21 +1,20 @@
-
 function updateCarousel(indexChange) {
-    const carouselContainer = document.querySelector('.carousel-container');
-    let currentIndex = parseInt(carouselContainer.dataset.currentIndex) || 0;
-    const totalSlides = carouselContainer.children.length;
-  
-    currentIndex = (currentIndex + indexChange + totalSlides) % totalSlides;
-  
-    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-    carouselContainer.dataset.currentIndex = currentIndex;
-  }
-  
-  export function nextSlide() {
-    updateCarousel(1);
-    console.log('nextSlide');
-  }
-  
-  export function prevSlide() {
-    updateCarousel(-1);
-    console.log('prevSlide');
-  }
+  const carouselContainer = document.querySelector('.carousel-container');
+  let currentIndex = parseInt(carouselContainer.dataset.currentIndex) || 0;
+  const totalSlides = carouselContainer.children.length;
+
+  currentIndex = (currentIndex + indexChange + totalSlides) % totalSlides;
+
+  carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  carouselContainer.dataset.currentIndex = currentIndex;
+}
+
+export function nextSlide() {
+  updateCarousel(1);
+  console.log('nextSlide');
+}
+
+export function prevSlide() {
+  updateCarousel(-1);
+  console.log('prevSlide');
+}
