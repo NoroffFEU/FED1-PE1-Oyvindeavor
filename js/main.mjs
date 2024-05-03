71026ee239dcb88220c5420108017cee68c4c3db
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-03 10:48:19 +0200

    Update main.mjs

--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,6 @@
 import { nextSlide, prevSlide } from "./components/carousel.mjs";
 import { doFetch } from "./utils/doFetch.mjs";
+import { hasAccessToken } from "./utils/hasAccessToken.mjs";
 
 // Name: oyvind
 // Email: oeyrii01001@stud.noroff.no
@@ -10,7 +11,75 @@ document.addEventListener("DOMContentLoaded", () => {
   /* Add event listeners to the hero carousel buttons */
   document.querySelector(".carousel-next-btn").addEventListener("click", nextSlide);
   document.querySelector(".carousel-prev-btn").addEventListener("click", prevSlide);
+
+  updateUI();
 });
+
+
+// Event listener if access token is stored
+
+// Checks to see if the access token is stored
+// If it is hide login button and create profile elements (profile picture, anchor element and logout button)
+function updateUI() {
+  if (hasAccessToken()) {
+    console.log("Acess token is stored");
+    hideLoginButton();
+    createProfileElements();
+  } else {
+    console.log("No access token stored");
+  }
+}
+
+function hideLoginButton() {
+  const loginButton = document.querySelector(".login-btn");
+  loginButton.style.display = "none";
+}
+
+function createProfileElements() {
+ // Create the image element
+const profilePicture = document.createElement("img");
+
+// Set the src attribute for the image
+profilePicture.src = "";
+
+// Create the anchor element
+const anchorElement = document.createElement("a");
+
+// Set the href attribute for the anchor element
+anchorElement.href = "/dashboard/index.html";
+
+// Append the image element to the anchor element
+anchorElement.appendChild(profilePicture);
+
+// Create logoutButton
+const logoutButton = document.createElement("button");
+
+logoutButton.textContent = "Log out";
+
+
+
+logoutButton.addEventListener("click", () => {
+  sessionStorage.removeItem("accessToken");
+  window.location.href = "/";
+});
+
+
+
+
+
+
+// Append the anchor element to a parent container
+const account = document.querySelector(".account");
+const profileInfo = document.querySelector(".profile-info");
+
+profileInfo.appendChild(anchorElement);
+profileInfo.appendChild(logoutButton);
+account.appendChild(profileInfo);
+
+}
+
+
+
 
 // Gets the status of the server
 async function getStatus() {
@@ -106,8 +175,8 @@ async function postBlogPost() {
     const response = await doFetch("/blog/posts/oyvind", {
       method: "POST",
       body: JSON.stringify({
-        title: "My first blog post",
-        body: "This is my first blog post. I hope you like it.",
+        title: "4",
+        body: "4",
         media: {
           url: "https://images.unsplash.com/photo-1711994872181-1e112e5e18e0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8",
           type: "image/jpeg"
@@ -186,5 +255,25 @@ async function getIndividualBlogPost() {
 }
 
 
+async function getCarouselImages() {
+  try {
+    const response = await doFetch(`/blog/posts/oyvind?sort=created&limit=3`);
+    console.log(response.data);
+    const carouselImg = document.querySelector('.carouselImg');
+    response.data.forEach((post) => {
+      console.log(post.media);
+      console.log(post.title);
+      carouselImg.src = post.media.url;
+      carouselImg.alt = post.media.alt;
+    });
+    
+  } catch (error) {
+    console.error("Error fetching carousel images:", error);
+    // Add a user-friendly error message here
+    alert("Error fetching carousel images");
+  }
+}
+
+//getCarouselImages();
 
 
