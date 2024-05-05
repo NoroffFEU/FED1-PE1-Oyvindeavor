e2c47bb57844925aefb9fd5969a3756445e162ea
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-05 18:37:45 +0200

    Update registerForm.mjs

--- js/registerForm.mjs
+++ js/registerForm.mjs
@@ -1,46 +1,46 @@
 import { doFetch } from "./utils/doFetch.mjs";
 
 function registerForm() {
-    const bio = document.getElementById("bio");
-    const profilePic = document.getElementById("profile-picture");
-    const name = document.getElementById("username");
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
-    const submitButton = document.getElementById("registerbutton");
+  const bio = document.getElementById("bio");
+  const profilePic = document.getElementById("profile-picture");
+  const name = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const submitButton = document.getElementById("registerbutton");
 
-    submitButton.addEventListener("click", async (event) => {
-        event.preventDefault();
-        try {
-            const response = await doFetch("/auth/register", {
-                method: "POST",
-                body: JSON.stringify({
-                    name: name.value,
-                    email: email.value,
-                    password: password.value,
-                    bio: bio.value,
-                    avatar:{
-                        url: profilePic.value,
-                    }
-                }),
-                
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            console.log(response);
-        } catch (error) {
-            console.error("Error registering:", error);
-            alert("Error registering");
-        }
-    });
+  submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    try {
+      const response = await doFetch("/auth/register", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name.value,
+          email: email.value,
+          password: password.value,
+          bio: bio.value,
+          avatar: {
+            url: profilePic.value,
+          },
+        }),
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.error("Error registering:", error);
+      alert("Error registering");
+    }
+  });
 }
 
 function profilePicPreview() {
-    const profilePic = document.getElementById("profile-picture");
-    const profilePicPreview = document.getElementById("profile-picture-preview");
+  const profilePic = document.getElementById("profile-picture");
+  const profilePicPreview = document.getElementById("profile-picture-preview");
 
-    profilePic.addEventListener("input", () => {
-        profilePicPreview.src = profilePic.value;
-    });
+  profilePic.addEventListener("input", () => {
+    profilePicPreview.src = profilePic.value;
+  });
 }
 profilePicPreview();
