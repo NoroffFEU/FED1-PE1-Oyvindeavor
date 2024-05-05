8c2bf894c0a2b4a9a3721f0cc0defd5155c7f244
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-05 18:37:38 +0200

    Update dashboard.mjs

--- js/dashboard.mjs
+++ js/dashboard.mjs
@@ -1,8 +1,9 @@
 import { clearAccessToken } from "./utils/clearAccessToken.mjs";
-import { doFetch } from "./utils/doFetch.mjs";
+import { redirectHome } from "./utils/redirectHome.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   const logOutButton = document.querySelector(".logout-btn");
+  displayUsername();
   // event listener to the logout button
   logOutButton.addEventListener("click", () => {
     clearAccessToken();
@@ -10,22 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function redirectHome() {
-  window.location.href = "../index.html";
-}
-
 function displayUsername() {
   const username = sessionStorage.getItem("userName");
   const usernameElement = document.querySelector(".userName");
   usernameElement.textContent = `Welcome back ${username}`;
 }
 
-displayUsername();
-
 function displayAvatar() {
-  const avatar = sessionStorage.getItem("avatar");
-  const avatarElement = document.querySelector(".avatar");
-  avatarElement.src = avatar;
+  let avatar = sessionStorage.getItem("avatar");
+  const avatarElement = document.querySelector("#profile-picture");
+  if (avatar === null) {
+    avatar = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"; // Add a default avatar here later !!
+  } else {
+    avatar = sessionStorage.getItem("avatar");
+    avatarElement.src = avatar;
+  }
 }
 
 displayAvatar();
