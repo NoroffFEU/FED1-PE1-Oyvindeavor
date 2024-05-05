807a80779736b17c2478de8e139f6853e576957e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-05 18:37:51 +0200

    Update hasAccessToken.mjs

--- js/utils/hasAccessToken.mjs
+++ js/utils/hasAccessToken.mjs
@@ -1,11 +1,11 @@
 // Checks to see if there is an access token stored in the browser
 function hasAccessToken() {
-    // Check if 'accessToken' exists in session storage
-    if (sessionStorage.getItem('accessToken') !== null) {
-        return true; // Access token exists
-    } else {
-        return false; // Access token does not exist
-    }
+  // Check if 'accessToken' exists in session storage
+  if (sessionStorage.getItem("accessToken") !== null) {
+    return true; // Access token exists
+  } else {
+    return false; // Access token does not exist
+  }
 }
 
 export { hasAccessToken };
