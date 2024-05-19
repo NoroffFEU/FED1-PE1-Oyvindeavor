de69f769e1220d4f9cee0dc3d9569431c15b9bf5
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-19 20:41:53 +0200

    Update doFetch.mjs

--- js/utils/doFetch.mjs
+++ js/utils/doFetch.mjs
@@ -2,6 +2,7 @@ import { apiUrl } from "/js/constants.mjs";
 
 async function doFetch(endpoint, options) {
   try {
+    showSpinner();
     const url = apiUrl + endpoint;
 
     const response = await fetch(url, options);
@@ -13,4 +14,27 @@ async function doFetch(endpoint, options) {
     throw new Error("Error fetching data"); // Throwing error for handling in calling function
   }
 }
+hideSpinner();
 export { doFetch };
+
+function showSpinner() {
+  document.body.setAttribute("loading", "true");
+}
+
+function hideSpinner() {
+  document.body.removeAttribute("loading");
+}
+
+async function doDelete() {
+  try {
+    const response = await fetch("/blog/posts/oyvind/1", {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    console.error("Error deleting post:", error);
+  }
+}
