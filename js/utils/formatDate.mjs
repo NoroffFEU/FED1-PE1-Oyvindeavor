b27995f2f5816c6796123444f4430546507d9505
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-19 20:41:57 +0200

    Create formatDate.mjs

---
+++ js/utils/formatDate.mjs
@@ -0,0 +1,6 @@
+function formatDate(date) {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(date).toLocaleDateString("en-us", options);
+  }
+
+  export { formatDate };
