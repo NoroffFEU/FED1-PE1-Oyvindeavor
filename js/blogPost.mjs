03c39799824da9af18dcd0b6a33fdb87d5fdfc73
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-21 20:23:53 +0200

    Update blogPost.mjs

--- js/blogPost.mjs
+++ js/blogPost.mjs
@@ -1,45 +1,38 @@
 import { updateUI } from "./utils/updateUi.mjs";
 import { doFetch } from "./utils/doFetch.mjs";
 import { formatDate } from "./utils/formatDate.mjs";
+import { fetchIdFromUrl } from "./utils/fetchIdFromUrl.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   updateUI();
+  displayBlogPost();
 });
-
 
 // Fetches data by id from the API and returns it
 async function fetchDataById() {
- try {
-   const id = fetchIdFromUrl();
- const response = await doFetch(`/blog/posts/oyvind/${id}`);
- return response.data;
- 
- } catch (error) {
-  console.error("Error fetching post:", error);
- }
-}
-
-// Searches the url for the id parameter and returns it
-function fetchIdFromUrl(){
-  const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get('id');
-  return id;
+  try {
+    const id = fetchIdFromUrl();
+    const response = await doFetch(`/blog/posts/oyvind/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching post:", error);
+  }
 }
 
 async function displayBlogPost() {
   const data = await fetchDataById();
   console.log(data);
 
-  const image = document.querySelector('.blog-image');
-  const title = document.querySelector('.article-title');
-  const content = document.querySelector('.article-description');
+  const image = document.querySelector(".blog-image");
+  const title = document.querySelector(".article-title");
+  const content = document.querySelector(".article-description");
 
-  const authorName = document.querySelector('.author-name');
-  const authorPicture = document.querySelector('.author-picture');
-  const authorBio = document.querySelector('.author-bio');
+  const authorName = document.querySelector(".author-name");
+  const authorPicture = document.querySelector(".author-picture");
+  const authorBio = document.querySelector(".author-bio");
 
-  const publishedDate = document.querySelector('.published-date');
-  const blogCategory = document.querySelector('.blog-category p');
+  const publishedDate = document.querySelector(".published-date");
+  const blogCategory = document.querySelector(".blog-category p");
 
   image.src = data.media.url;
   title.textContent = data.title;
@@ -51,8 +44,3 @@ async function displayBlogPost() {
   authorPicture.alt = data.author.avatar.alt;
   blogCategory.textContent = data.tags[0];
 }
-
-
-fetchDataById();
-displayBlogPost();
-
