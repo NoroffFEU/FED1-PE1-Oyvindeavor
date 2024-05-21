ee737e5fae786d02830e114dfe4492f71f91c3be
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-21 20:23:54 +0200

    Update carousel.mjs

--- js/components/carousel.mjs
+++ js/components/carousel.mjs
@@ -2,9 +2,7 @@ function updateCarousel(indexChange) {
   const carouselContainer = document.querySelector('.carousel-container');
   let currentIndex = parseInt(carouselContainer.dataset.currentIndex) || 0;
   const totalSlides = carouselContainer.children.length;
-
   currentIndex = (currentIndex + indexChange + totalSlides) % totalSlides;
-
   carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
   carouselContainer.dataset.currentIndex = currentIndex;
 }
