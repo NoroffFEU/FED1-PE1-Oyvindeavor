1a0b139c5330700b13252279c379606e525f9e87
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-21 20:24:05 +0200

    Update loginForm.mjs

--- js/loginForm.mjs
+++ js/loginForm.mjs
@@ -2,22 +2,17 @@
 import { login } from "./login.mjs";
 
 function handleLoginFormSubmit(event) {
-  event.preventDefault(); // Prevent the default form submission
-
-  // Call the login function
+  event.preventDefault(); 
   login();
 }
 
-// Attach event listener to the form
 document.querySelector(".login-form").addEventListener("submit", handleLoginFormSubmit);
 
-// Reads the value of the email input field
 function emailInput() {
   return document.getElementById("email").value;
 }
 export { emailInput };
 
-// Reads the value of the password input field
 function passwordInput() {
   return document.getElementById("password").value;
 }
