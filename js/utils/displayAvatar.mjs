ffdb9ce2fb996007ea0b7c11fe849d69bb6af923
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-21 20:24:10 +0200

    Update displayAvatar.mjs

--- js/utils/displayAvatar.mjs
+++ js/utils/displayAvatar.mjs
@@ -2,7 +2,7 @@ function displayAvatar() {
     let avatar = sessionStorage.getItem("avatar");
     const avatarElement = document.querySelector(".profile-picture");
     if (avatar === null) {
-      avatar = "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"; // Add a default avatar here later !!
+      avatar = "../assets/assets/account_circle_24dp_FILL0_wght100_GRAD200_opsz48.svg"; // Add a default avatar here later !!
     } else {
       avatar = sessionStorage.getItem("avatar");
       avatarElement.src = avatar;
