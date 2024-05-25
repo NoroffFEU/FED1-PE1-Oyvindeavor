adc496ebed0a7514a08626d729bd2c50544158ac
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-25 18:46:38 +0200

    Update filter.mjs

--- js/utils/filter.mjs
+++ js/utils/filter.mjs
@@ -1,12 +1,12 @@
 async function filterByCategory(category) {
-    try {
-      const response = await doFetch(`/blog/posts/oyvind?_tag${category}`);
-      console.log(response);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      throw error;
-    }
+  try {
+    const response = await doFetch(`/blog/posts/oyvind?_tag${category}`);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error;
   }
+}
 
-  export { filterByCategory };
+export { filterByCategory };
