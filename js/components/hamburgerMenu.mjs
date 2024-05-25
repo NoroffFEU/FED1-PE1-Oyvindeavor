681af3ee8c481703d652f2f71b36b48b544a434e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-25 18:46:00 +0200

    Update hamburgerMenu.mjs

--- js/components/hamburgerMenu.mjs
+++ js/components/hamburgerMenu.mjs
@@ -1,15 +1,33 @@
+import { hasAccessToken } from "../utils/hasAccessToken.mjs";
+import { clearAccessToken } from "../utils/clearAccessToken.mjs";
+
 function hamburgerMenu() {
-    const menuButton = document.querySelector('.hamburger-menu-button');
-    const closeButton = document.querySelector('.close-menu-button');
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const menuButton = document.querySelector(".hamburger-menu-button");
+  console.log(menuButton);
+  const sideMenu = document.querySelector(".side-menu");
+  const body = document.body;
+  const main = document.querySelector("main");
+  const footer = document.querySelector("footer");
+  const header = document.querySelector("header");
 
-    menuButton.addEventListener('click', function() {
-        hamburgerMenu.classList.add('open'); // Open the menu
-    });
-
-    closeButton.addEventListener('click', function() {
-        hamburgerMenu.classList.remove('open'); // Close the menu
-    });
+  menuButton.addEventListener("click", function () {
+    console.log("click");
+    if (sideMenu.classList.contains("show")) {
+      // Close menu
+      sideMenu.classList.remove("show");
+      body.classList.remove("no-scroll");
+      main.classList.remove("opacity-blur");
+      footer.classList.remove("opacity-blur");
+      header.classList.remove("opacity-blur");
+    } else {
+      // Open menu
+      sideMenu.classList.add("show");
+      body.classList.add("no-scroll");
+      main.classList.add("opacity-blur");
+      footer.classList.add("opacity-blur");
+      header.classList.add("opacity-blur");
+    }
+  });
 }
 
 export { hamburgerMenu };
