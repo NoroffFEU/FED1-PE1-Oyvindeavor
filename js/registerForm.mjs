0611ce6f7c585e45877ffb4af042b238aafede01
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-27 07:33:25 +0200

    Update registerForm.mjs

--- js/registerForm.mjs
+++ js/registerForm.mjs
@@ -3,13 +3,11 @@ import { updateUI } from "./utils/updateUi.mjs";
 import { hamburgerMenu } from "./components/hamburgerMenu.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
-  clearForm();
   updateUI();
-  registerForm();
   hamburgerMenu();
 });
 
-function registerForm() {
+async function registerForm() {
   const bio = document.getElementById("bio");
   const profilePic = document.getElementById("profile-picture");
   const name = document.getElementById("username");
@@ -18,78 +16,58 @@ function registerForm() {
   const submitButton = document.getElementById("registerbutton");
 
   submitButton.addEventListener("click", async (event) => {
-    event.preventDefault();
+    event.preventDefault(); 
+
+    // Check each input for validity
+    if (!name.checkValidity() || !email.checkValidity() || !password.checkValidity() || !bio.checkValidity() || !profilePic.checkValidity() ){
+      email.reportValidity(); 
+      password.reportValidity();
+      name.reportValidity(); 
+      bio.reportValidity(); 
+      profilePic.reportValidity(); 
+      return; 
+    }
+
     try {
-      const avatar = profilePic.value ? { url: profilePic.value } : null;
-      const requestBody = {
-        name: name.value,
-        email: email.value,
-        password: password.value,
-      };
 
-      if (bio.value) {
-        requestBody.bio = bio.value;
-      }
-
-      if (avatar) {
-        requestBody.avatar = avatar;
-      }
-
-      const response = await doFetch("/auth/register/", {
+      // Send the registration data to the server
+      const response = await doFetch("/auth/register", {
         method: "POST",
-        body: JSON.stringify(requestBody),
+        body: JSON.stringify({
+          name: name.value,
+          email: email.value,
+          password: password.value,
+          bio: bio.value,
+          avatar: {
+            url: profilePic.value,
+            alt: name.value,
+          }
+        }),
         headers: {
           "Content-Type": "application/json",
         },
       });
-
+      
       if (response.ok) {
-        alert("User registered successfully");
-        window.location.href = `../dashboard/index.html`;
-      } else {
-        console.error("Error registering:", response.errors[0].message);
-        alert("Error registering: " + response.errors[0].message);
+        window.location.href = "../dashboard/index.html";
+      }
+      else {
+        console.error("Error registering:",);
+        alert("Error registering");
       }
     } catch (error) {
-      console.error("Error registering:", error.message);
-      alert("Error registering: " + error.message);
+      console.error("Error registering:", error);
+      alert("Error registering");
     }
+
   });
 }
 
-function nameInputValidation() {
-  const name = document.getElementById("username");
-  const nameError = document.getElementById("username-error");
-
-  name.addEventListener("input", () => {
-    // Regular expression for valid characters
-    const validNamePattern = /^[a-zA-Z0-9_]+$/;
-
-    if (!name.value) {
-      nameError.textContent = "Name is required";
-      nameError.style.display = "block";
-    } else if (!validNamePattern.test(name.value)) {
-      nameError.textContent = "Name can only include letters, numbers, and underscores";
-      nameError.style.display = "block";
-    } else {
-      nameError.textContent = "";
-      nameError.style.display = "none";
-    }
-  });
-}
+registerForm();
 
 
-function clearForm() {
-  document.getElementById("bio").value = "";
-  document.getElementById("profile-picture").value = "";
-  document.getElementById("username").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
-}
 
-nameInputValidation();
 
-// Profile pic preview so the user can see the image they input before sending the form
 function profilePicPreview() {
   const profilePic = document.getElementById("profile-picture");
   const profilePicPreview = document.getElementById("profile-picture-preview");
@@ -103,4 +81,5 @@ function profilePicPreview() {
     profilePicPreview.style.display = "block";
   });
 }
+
 profilePicPreview();
