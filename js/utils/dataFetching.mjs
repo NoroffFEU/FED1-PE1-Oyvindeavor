c98b38575adacd4b64850d0256d49d6af51ef608
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:38:14 +0200

    Create dataFetching.mjs

---
+++ js/utils/dataFetching.mjs
@@ -0,0 +1,27 @@
+import { doFetch } from "./doFetch.mjs";
+import { apiUrl, blogName } from "../constants.mjs";
+
+export async function getPosts(queryParams) {
+    const response = await doFetch(`${apiUrl}/blog/posts/${blogName}?${queryParams}`);
+    return response.data || [];
+}
+
+export async function getThreeLatestPosts() {
+    return getPosts("limit=3&sortOrder=desc");
+}
+
+
+
+export async function getFilteredPosts(sort, filter) {
+    let queryParams = 'limit=12'; 
+    if (sort) {
+        queryParams += `&sortOrder=${encodeURIComponent(sort)}`;
+    }
+    if (filter) {
+        queryParams += `&_tag=${encodeURIComponent(filter)}`;
+    }
+    return getPosts(queryParams);
+}
+
+
+
