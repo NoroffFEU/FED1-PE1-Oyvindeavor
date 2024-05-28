8d395962da59271812276f50207addcadc019d24
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:38:12 +0200

    Update registerForm.mjs

--- js/registerForm.mjs
+++ js/registerForm.mjs
@@ -1,10 +1,12 @@
-import { doFetch } from "./utils/doFetch.mjs";
+
 import { updateUI } from "./utils/updateUi.mjs";
 import { hamburgerMenu } from "./components/hamburgerMenu.mjs";
+import { apiUrl, blogName } from "./constants.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
   updateUI();
   hamburgerMenu();
+  registerForm();
 });
 
 async function registerForm() {
@@ -14,72 +16,55 @@ async function registerForm() {
   const email = document.getElementById("email");
   const password = document.getElementById("password");
   const submitButton = document.getElementById("registerbutton");
-
   submitButton.addEventListener("click", async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
     // Check each input for validity
-    if (!name.checkValidity() || !email.checkValidity() || !password.checkValidity() || !bio.checkValidity() || !profilePic.checkValidity() ){
-      email.reportValidity(); 
+    if (!name.checkValidity() || !email.checkValidity() || !password.checkValidity() || !bio.checkValidity() || !profilePic.checkValidity()) {
+      email.reportValidity();
       password.reportValidity();
-      name.reportValidity(); 
-      bio.reportValidity(); 
-      profilePic.reportValidity(); 
-      return; 
+      name.reportValidity();
+      bio.reportValidity();
+      profilePic.reportValidity();
+      return;
     }
 
     try {
-
       // Send the registration data to the server
-      const response = await doFetch("/auth/register", {
+      const response = await fetch(`${apiUrl}/auth/register`, {
         method: "POST",
         body: JSON.stringify({
           name: name.value,
           email: email.value,
-          password: password.value,
           bio: bio.value,
           avatar: {
             url: profilePic.value,
-            alt: name.value,
-          }
+            alt: name.value, 
+          },
+          password: password.value,
         }),
         headers: {
           "Content-Type": "application/json",
+          authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
         },
       });
-      
+
       if (response.ok) {
+        const sucessResponse = await response.json();
+        alert(`Sucessfully created account: ${sucessResponse.data.name}`);
         window.location.href = "../dashboard/index.html";
-      }
-      else {
-        console.error("Error registering:",);
-        alert("Error registering");
+        // window.location.href = "../dashboard/index.html";
+      } else {
+        const responseData = await response.json();
+        console.error("Error registering:", responseData);
+        alert("Error registering: " + responseData.message);
       }
     } catch (error) {
       console.error("Error registering:", error);
-      alert("Error registering");
+      alert("Error registering: " + error.message);
     }
-
   });
 }
 
-registerForm();
 
 
-
-
-function profilePicPreview() {
-  const profilePic = document.getElementById("profile-picture");
-  const profilePicPreview = document.getElementById("profile-picture-preview");
-
-  profilePic.addEventListener("input", () => {
-    if (!profilePic.value) {
-      profilePicPreview.style.display = "none";
-      return;
-    }
-    profilePicPreview.src = profilePic.value;
-    profilePicPreview.style.display = "block";
-  });
-}
-
-profilePicPreview();
