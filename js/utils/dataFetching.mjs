8e025d905f96202f5086bd4f5e7a5b4d24d50348
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 22:07:06 +0200

    Update dataFetching.mjs

--- js/utils/dataFetching.mjs
+++ js/utils/dataFetching.mjs
@@ -2,26 +2,21 @@ import { doFetch } from "./doFetch.mjs";
 import { apiUrl, blogName } from "../constants.mjs";
 
 export async function getPosts(queryParams) {
-    const response = await doFetch(`${apiUrl}/blog/posts/${blogName}?${queryParams}`);
-    return response.data || [];
+  const response = await doFetch(`${apiUrl}/blog/posts/${blogName}?${queryParams}`);
+  return response.data || [];
 }
 
 export async function getThreeLatestPosts() {
-    return getPosts("limit=3&sortOrder=desc");
+  return getPosts("limit=3&sortOrder=desc");
 }
-
-
 
 export async function getFilteredPosts(sort, filter) {
-    let queryParams = 'limit=12'; 
-    if (sort) {
-        queryParams += `&sortOrder=${encodeURIComponent(sort)}`;
-    }
-    if (filter) {
-        queryParams += `&_tag=${encodeURIComponent(filter)}`;
-    }
-    return getPosts(queryParams);
+  let queryParams = "limit=12";
+  if (sort) {
+    queryParams += `&sortOrder=${encodeURIComponent(sort)}`;
+  }
+  if (filter) {
+    queryParams += `&_tag=${encodeURIComponent(filter)}`;
+  }
+  return getPosts(queryParams);
 }
-
-
-
