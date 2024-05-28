1d50a96d1dbcdfcbfdaba8d40c08647e4e4d6898
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:38:25 +0200

    Update filter.mjs

--- js/utils/filter.mjs
+++ js/utils/filter.mjs
@@ -1,6 +1,8 @@
+import { apiUrl, blogName } from "../constants.mjs";
+
 async function filterByCategory(category) {
   try {
-    const response = await doFetch(`/blog/posts/oyvind?_tag${category}`);
+    const response = await doFetch(`${apiUrl}/blog/posts/${blogName}?_tag${category}`);
     console.log(response);
     return response.data;
   } catch (error) {
