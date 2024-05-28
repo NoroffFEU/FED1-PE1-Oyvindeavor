4912943b1a5a74f205acf5166466c56a3d5fcb12
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 21:21:03 +0200

    Update storeUserName.mjs

--- js/utils/storeUserName.mjs
+++ js/utils/storeUserName.mjs
@@ -1,7 +1,7 @@
 function storeUserName(response) {
   const userName = response.data.name;
   sessionStorage.setItem("userName", userName);
-  console.log("Name:", response.data.name);
+
 }
 
 export { storeUserName };
