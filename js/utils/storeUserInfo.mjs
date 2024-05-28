fab8c8e20dc0b7963c763039790a83dbabdb9917
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 22:07:18 +0200

    Update storeUserInfo.mjs

--- js/utils/storeUserInfo.mjs
+++ js/utils/storeUserInfo.mjs
@@ -1,19 +1,28 @@
 export function storeUserName(response) {
-    const userName = response.data.name;
-    sessionStorage.setItem("userName", userName);
+  const userName = response.data.name;
+  sessionStorage.setItem("userName", userName);
+}
 
-  }
-  
+export function storeBio(response) {
+  const bio = response.data.bio;
+  sessionStorage.setItem("bio", bio);
+}
 
-  export function storeBio(response) {
-    const bio = response.data.bio;
-    sessionStorage.setItem("bio", bio);
+export function storeAccessToken(response) {
+  const accessToken = response.data.accessToken;
+  sessionStorage.setItem("accessToken", accessToken);
+}
 
-  }
-  
-  export function storeAvatar(response) {
-    const avatar = response.data.avatar.url;
-    sessionStorage.setItem("avatar", avatar);
-  }
+export function storeAvatar(response) {
+  const avatar = response.data.avatar.url;
+  const avatarAlt = response.data.avatar.alt;
+  sessionStorage.setItem("avatar", avatar);
+  sessionStorage.setItem("avatarAlt", avatarAlt);
+}
 
- 
+export function storeUserInfo(response) {
+  storeAccessToken(response);
+  storeUserName(response);
+  storeBio(response);
+  storeAvatar(response);
+}
