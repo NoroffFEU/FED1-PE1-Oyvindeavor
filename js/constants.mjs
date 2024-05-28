a6003cd404a5351101cb69759c4dca57182b2c8f
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:37:59 +0200

    Update constants.mjs

--- js/constants.mjs
+++ js/constants.mjs
@@ -3,4 +3,8 @@ const noroffAPIURL = "https://v2.api.noroff.dev";
 
 // use a different constant for usage so that the main constant is not changed by mistake
 const apiUrl = noroffAPIURL;
-export { apiUrl };
+
+// If this is changed it will display a different blog (maybe the user want a new blog or something)
+const blogName = "oyvind";
+
+export { apiUrl, blogName};
