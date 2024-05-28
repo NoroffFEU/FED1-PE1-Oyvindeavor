49019f398fdfc70398d9eb54323486eeef59be9b
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:38:18 +0200

    Update doFetch.mjs

--- js/utils/doFetch.mjs
+++ js/utils/doFetch.mjs
@@ -1,11 +1,25 @@
 import { apiUrl } from "../constants.mjs";
 import { handleErrors } from "../utils/handleErrors.mjs";
 
+// async function doFetch(endpoint, options) {
+//   try {
+
+//     const url = apiUrl + endpoint;
+
+//     const response = await fetch(url, options);
+
+//     return response.json();
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw new Error("Error fetching data");
+//   }
+// }
+
 async function doFetch(endpoint, options) {
   try {
-    showSpinner();
-    const url = apiUrl + endpoint;
 
+
+    const url = endpoint;
     const response = await fetch(url, options);
 
     return response.json();
@@ -14,13 +28,6 @@ async function doFetch(endpoint, options) {
     throw new Error("Error fetching data");
   }
 }
-hideSpinner();
+
 export { doFetch };
 
-function showSpinner() {
-  document.body.setAttribute("loading", "true");
-}
-
-function hideSpinner() {
-  document.body.removeAttribute("loading");
-}
