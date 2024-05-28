a2e72c7f502947b0ee43edd61d9f05dd56b4da9b
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 21:20:53 +0200

    Update doFetch.mjs

--- js/utils/doFetch.mjs
+++ js/utils/doFetch.mjs
@@ -1,19 +1,4 @@
-import { apiUrl } from "../constants.mjs";
-import { handleErrors } from "../utils/handleErrors.mjs";
 
-// async function doFetch(endpoint, options) {
-//   try {
-
-//     const url = apiUrl + endpoint;
-
-//     const response = await fetch(url, options);
-
-//     return response.json();
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//     throw new Error("Error fetching data");
-//   }
-// }
 
 async function doFetch(endpoint, options) {
   try {
