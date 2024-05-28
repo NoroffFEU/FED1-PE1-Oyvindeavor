61d4b05ade220d7b481b3a41c1dcf5d1ba233f65
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 21:21:01 +0200

    Update storeUserInfo.mjs

--- js/utils/storeUserInfo.mjs
+++ js/utils/storeUserInfo.mjs
@@ -1,14 +1,14 @@
 export function storeUserName(response) {
     const userName = response.data.name;
     sessionStorage.setItem("userName", userName);
-    console.log("Name:", response.data.name);
+
   }
   
 
   export function storeBio(response) {
     const bio = response.data.bio;
     sessionStorage.setItem("bio", bio);
-    console.log("Bio: ", response.data.bio);
+
   }
   
   export function storeAvatar(response) {
