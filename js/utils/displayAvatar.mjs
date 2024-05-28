56574ff18d206d9bc3c47c0f8df4202f5394c681
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 19:55:50 +0200

    Update displayAvatar.mjs

--- js/utils/displayAvatar.mjs
+++ js/utils/displayAvatar.mjs
@@ -1,12 +1,29 @@
 function displayAvatar() {
+  // Retrieve the avatar URL and alternative text from sessionStorage
   let avatar = sessionStorage.getItem("avatar");
+  let alt = sessionStorage.getItem("avatarAlt");
+
+  // Select the element where the avatar will be displayed
   const avatarElement = document.querySelector(".profile-picture");
+  const avatarElementDashboard = document.querySelector(".profile-picture-dashboard");
+
+  // Check if the avatar URL is not stored; use a default if not
   if (avatar === null) {
-    avatar = "../assets/assets/account_circle_24dp_FILL0_wght100_GRAD200_opsz48.svg"; 
-  } else {
-    avatar = sessionStorage.getItem("avatar");
-    avatarElement.src = avatar;
+    avatar = "../assets/account_circle_24dp_FILL0_wght100_GRAD200_opsz48.svg"; 
+    avatar.alt = "Default Avatar"; 
   }
+
+  // Update the image source and alternative text
+  avatarElement.src = avatar;
+  avatarElement.alt = alt;
+  if (avatarElementDashboard === null) {
+    return;
+  }
+  avatarElementDashboard.src = avatar;
+  avatarElementDashboard.alt = alt;
+  console.log("Avatar displayed", avatarElement.src);
+  console.log("avatar alt text", avatarElement.alt);
 }
+
 
 export { displayAvatar };
