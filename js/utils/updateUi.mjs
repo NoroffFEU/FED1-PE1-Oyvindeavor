81b6f1a3b37b1ad1aadb7005a42635a3649fabf8
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 07:38:32 +0200

    Update updateUi.mjs

--- js/utils/updateUi.mjs
+++ js/utils/updateUi.mjs
@@ -1,3 +1,6 @@
+// This file contains functions that update the UI based on whether the user is logged in or not 
+// and also contains functions that show and hide elements on the page based on the user's login status.
+
 import { clearAccessToken } from "./clearAccessToken.mjs";
 import { redirectHome } from "./redirectHome.mjs";
 import { hasAccessToken } from "./hasAccessToken.mjs";
@@ -7,32 +10,34 @@ function updateUI() {
   if (hasAccessToken()) {
     // Checks to see if the access token is stored (if it is do the following)
     console.log("Access token is stored");
+    hideLoginButtonHamburger();
     displayAvatar();
-    hideLoginButton();
-    showLogoutButton();
+    changeLogInButton();
+    handleLogoutEvent();
     addRedirectToAvatar(); // Adds an href to the anchor element and redirects to the dashboard when clicked
   } else {
+    hideAdminButtonsHamburger();
     // If the access token is not stored (do the following)
     console.log("No access token stored");
-    showLoginButton();
-    hideLogoutButton();
+
   }
 }
 export { updateUI };
 
-function hideLoginButton() {
+function changeLogInButton() {
   const loginButton = document.querySelector(".login-btn");
-  loginButton.style.display = "none";
+  loginButton.textContent = "Logout";
+  loginButton.href = "../index.html";
+  loginButton.addEventListener("click", () => {
+    clearAccessToken();
+    redirectHome();
+  }
+  );
 }
 
-function showLoginButton() {
-  const loginButton = document.querySelector(".login-btn");
-  loginButton.style.display = "block";
-}
 
-function showLogoutButton() {
-  const logoutButton = document.querySelector(".logout-btn");
-  logoutButton.style.display = "block";
+function handleLogoutEvent() {
+  const logoutButton = document.querySelector(".login-btn");
 
   logoutButton.addEventListener("click", () => {
     clearAccessToken();
@@ -40,9 +45,28 @@ function showLogoutButton() {
   });
 }
 
-function hideLogoutButton() {
-  const logoutButton = document.querySelector(".logout-btn");
+function hideLoginButtonHamburger() {
+  const logoutButton = document.querySelector("#logout-hamburger");
+  const dashboardButton = document.querySelector("#dashboard-hamburger");
+
+  const loginButton = document.querySelector("#login-hamburger");
+  loginButton.style.display = "none";
+  logoutButton.style.display = "block";
+  dashboardButton.style.display = "block";
+
+  logoutButton.addEventListener("click", () => {
+    clearAccessToken();
+    redirectHome();
+  });
+}
+
+function hideAdminButtonsHamburger() {
+  const logoutButton = document.querySelector("#logout-hamburger");
+  const dashboardButton = document.querySelector("#dashboard-hamburger");
+
   logoutButton.style.display = "none";
+  dashboardButton.style.display = "none";
+
 }
 
 // Adds a redirect to the dashboard when clicked on profile picture in the header
