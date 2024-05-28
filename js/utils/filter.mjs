b593bb651fb5ae4b5b86549899179154336d2ea3
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-05-28 21:20:57 +0200

    Update filter.mjs

--- js/utils/filter.mjs
+++ js/utils/filter.mjs
@@ -3,7 +3,7 @@ import { apiUrl, blogName } from "../constants.mjs";
 async function filterByCategory(category) {
   try {
     const response = await doFetch(`${apiUrl}/blog/posts/${blogName}?_tag${category}`);
-    console.log(response);
+
     return response.data;
   } catch (error) {
     console.error("Error fetching posts:", error);
